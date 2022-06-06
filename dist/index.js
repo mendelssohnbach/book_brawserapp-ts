fd8d713ee067123e77c0c03864ea263e9376bd30
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-06 09:16:19 +0900

    :sparkles: HTML要素の取得

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,19 @@ var __webpack_exports__ = {};
   !*** ./ts/index.ts ***!
   \*********************/
 
-console.log('Hello World');
+class Application {
+    start() {
+        const button = document.getElementById('deleteAndDoneTask');
+        if (!button) {
+            console.log(button);
+        }
+    }
+}
+window.addEventListener('load', () => {
+    const app = new Application();
+    app.start();
+});
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9icmF3c2VyLWFwcC8uL3RzL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZWxsbyBXb3JsZCcpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLE1BQU0sV0FBVztJQUNmLEtBQUs7UUFDSCxNQUFNLE1BQU0sR0FBRyxRQUFRLENBQUMsY0FBYyxDQUFDLG1CQUFtQixDQUFDLENBQUM7UUFFNUQsSUFBSSxDQUFDLE1BQU0sRUFBRTtZQUNYLE9BQU8sQ0FBQyxHQUFHLENBQUMsTUFBTSxDQUFDLENBQUM7U0FDckI7SUFDSCxDQUFDO0NBQ0Y7QUFFRCxNQUFNLENBQUMsZ0JBQWdCLENBQUMsTUFBTSxFQUFFLEdBQUcsRUFBRTtJQUNuQyxNQUFNLEdBQUcsR0FBRyxJQUFJLFdBQVcsRUFBRSxDQUFDO0lBQzlCLEdBQUcsQ0FBQyxLQUFLLEVBQUUsQ0FBQztBQUNkLENBQUMsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYnJhd3Nlci1hcHAvLi90cy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBBcHBsaWNhdGlvbiB7XG4gIHN0YXJ0KCkge1xuICAgIGNvbnN0IGJ1dHRvbiA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdkZWxldGVBbmREb25lVGFzaycpO1xuXG4gICAgaWYgKCFidXR0b24pIHtcbiAgICAgIGNvbnNvbGUubG9nKGJ1dHRvbik7XG4gICAgfVxuICB9XG59XG5cbndpbmRvdy5hZGRFdmVudExpc3RlbmVyKCdsb2FkJywgKCkgPT4ge1xuICBjb25zdCBhcHAgPSBuZXcgQXBwbGljYXRpb24oKTtcbiAgYXBwLnN0YXJ0KCk7XG59KTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+class Application {
+  start() {
+    const button = document.getElementById('deleteAndDoneTask');
+
+    if (!button) {
+      console.log(button);
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  const app = new Application();
+  app.start();
+});
