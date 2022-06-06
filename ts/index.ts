df920b4dbf9c7da5b15e524e5708e1a0779e2359
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-07 06:18:59 +0900

    :sparkles: handleDropAndDropメソッド追加

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,5 @@
 import { EventListener } from './EventListener';
-import { Task } from './Task';
+import { Status, Task } from './Task';
 import { TaskCollection } from './TaskCollection';
 import { TaskRenderer } from './TaskRenderer';
 
@@ -17,7 +17,7 @@ class Application {
 
     this.eventListener.add('submit-handler', 'submit', createForm, this.handleSubmit);
 
-    this.taskRenderer.subscribeDragAndDrop();
+    this.taskRenderer.subscribeDragAndDrop(this.handleDropAndDrop);
   }
 
   private handleSubmit = (e: Event) => {
@@ -45,6 +45,16 @@ class Application {
     this.eventListener.remove(task.id);
     this.taskCollection.delete(task);
     this.taskRenderer.remove(task);
+  };
+
+  private handleDropAndDrop = (el: Element, sibling: Element | null, newStatus: Status) => {
+    const taskId = this.taskRenderer.getId(el);
+
+    if (!taskId) return;
+
+    console.log('taskId: ', taskId);
+    console.log('sibling: ', sibling);
+    console.log('newStatus: ', newStatus);
   };
 }
 
