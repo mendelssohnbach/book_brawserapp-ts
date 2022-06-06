ffb8670d274177fee508f54830213a44661d5f95
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-06 16:33:22 +0900

    :sparkles: TaksRendererにdelteButtonEL追加

--- ts/TaskRenderer.ts
+++ ts/TaskRenderer.ts
@@ -4,9 +4,10 @@ export class TaskRenderer {
   constructor(private readonly todoList: HTMLElement) {}
 
   append(task: Task) {
-    const taskEl = this.render(task);
+    const { taskEl, deleteButtonEl } = this.render(task);
 
     this.todoList.append(taskEl);
+    return { deleteButtonEl };
   }
 
   private render(task: Task) {
@@ -27,6 +28,6 @@ export class TaskRenderer {
 
     taskEl.append(spanEl, deleteButtonEl);
 
-    return taskEl;
+    return { taskEl, deleteButtonEl };
   }
 }
