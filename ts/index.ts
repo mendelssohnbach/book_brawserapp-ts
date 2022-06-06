8fb78a95afc59f95e67490da9c9b0e824878f41b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-06 16:41:41 +0900

    :sparkles: deleteButtonElにeventListener.add登録

--- ts/index.ts
+++ ts/index.ts
@@ -26,10 +26,23 @@ class Application {
     const task = new Task({ title: titleInput.value });
     this.taskCollection.add(task);
 
-    this.taskRenderer.append(task);
+    const {deleteButtonEl}=this.taskRenderer.append(task);
+
+    this.eventListener.add(
+      task.id,
+      'click',
+      deleteButtonEl,
+      () => this.handleClickDeleteTask(task);
+    )
 
     titleInput.value = '';
   };
+
+  private handleClickDeleteTask = (task: Task) => {
+    if (!window.confirm(`「${task.title}」を削除してよろしいですか？`)) return;
+
+    console.log(task)
+  }
 }
 
 window.addEventListener('load', () => {
